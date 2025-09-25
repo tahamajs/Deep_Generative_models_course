886e618e50b46adfe2acb5832a1ac3a51ac9fcea
taha <tahamaj4@gmail.com>
2025-09-25 23:02:28 +0330

    add run.sh

---
+++ CAs/CA4/code/run.sh
@@ -0,0 +1,9 @@
+# Install the tool
+pip install huggingface_hub
+
+# Log in and paste your token when prompted
+huggingface-cli login
+
+hf download google/paligemma-3b-mix-224 --local-dir paligemma-3b-mix-224
+
+
